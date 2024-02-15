a0a0689def9b9e80bdf67b90764b7215a78ed9b9
Rolob3rto <robertooli98@gmail.com>
2024-02-15 18:06:11 +0100

    commit inicial

---
+++ src/main/java/com/roberto/PruebaTecnica4/PruebaTecnica4Application.java
@@ -0,0 +1,13 @@
+package com.roberto.PruebaTecnica4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaTecnica4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaTecnica4Application.class, args);
+	}
+
+}
